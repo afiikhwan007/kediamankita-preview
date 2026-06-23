90d9f55e9e2e5e0c2eba1d67c87146dd0e298a88
afiikhwan007 <72002289+afiikhwan007@users.noreply.github.com>
2026-06-23 17:19:41 +0800

    Add files via upload

---
+++ typebot-bina-kelayakan.js
@@ -0,0 +1,9 @@
+import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0/dist/web.js'
+Typebot.initBubble({
+  typebot: "bina-kelayakan",
+  font: { type: "Google", family: "Poppins" },
+  theme: {
+    button: { backgroundColor: "#D8A838", iconColor: "#0E1C33" },
+    chatWindow: { backgroundColor: "#F5F7F9" },
+  },
+});
